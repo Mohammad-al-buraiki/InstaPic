74279bad9f6553415e80c56e7d2c593f1132f6b6
Mohammad Al-Buraiki <alburaeki@gmail.com>
2024-09-24 19:06:25 -0400

    feed posts is complete

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,8 +1,8 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, InputGroup, Input, Button, Avatar, HStack, VStack, Divider, InputRightElement } from "@chakra-ui/react";
 import { useState } from "react";
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from '../../assets/constants';
 
-const PostFooter = () => {
+const PostFooter = ({username}) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -17,8 +17,8 @@ const PostFooter = () => {
   };
 
   return (
-    <>
-      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={3}>
+    <Box mb={4}>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike}>
           {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
         </Box>
@@ -33,19 +33,34 @@ const PostFooter = () => {
       </Text>
 
       <Text fontSize="sm" fontWeight={700}>
-        asapprogrammer_{" "}
+        {username}{" "}
         <Text as='span' fontWeight={400}>
           Feeling good
         </Text>
       </Text>
 
       <Text fontSize="sm" color={"gray"}>
-        View all ### comments
+        View all 99 comments
       </Text>
 
-
-
-    </>
+      <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+        <InputGroup>
+          <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14} />
+          <InputRightElement>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={600}
+              cursor={"pointer"}
+              _hover={{ color: "white" }}
+              bg={"transparent"}
+            >
+              Post
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </Box>
   );
 };
 
