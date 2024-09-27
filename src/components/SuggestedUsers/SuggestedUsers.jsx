5f3d024b220b44425a3a83ee949514ad20d9b1d8
Mohammad Al-Buraiki <alburaeki@gmail.com>
2024-09-27 15:44:33 -0400

    suggested users functionality is complete

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,33 +1,36 @@
-import { VStack, Flex, Text, Box, Link,  } from "@chakra-ui/react";
-import SuggestedHeader from "./SuggestedHeader";
-import SuggestedUser from "./SuggestedUser";
+import { VStack, Flex, Text, Box, Link } from '@chakra-ui/react';
+import SuggestedHeader from './SuggestedHeader';
+import SuggestedUser from './SuggestedUser';
+import useGetSuggestedUsers from '../../hooks/useGetSuggestedUsers';
 const SuggestedUsers = () => {
-    return (
-      <VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
-  
-        <Flex alignItems="center" justifyContent={"space-between"} w={"full"}>
-          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+  const { isLoading, suggestedUsers } = useGetSuggestedUsers();
+  return (
+    <VStack py={8} px={6} gap={4}>
+      <SuggestedHeader />
+
+      {suggestedUsers.length !== 0 && (
+        <Flex alignItems='center' justifyContent={'space-between'} w={'full'}>
+          <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
             Suggested for you
           </Text>
-          <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+          <Text fontSize={12} fontWeight={'bold'} _hover={{ color: 'gray.400' }} cursor={'pointer'}>
             See All
           </Text>
         </Flex>
-  
-        <SuggestedUser name='Dan Abramov' followers={1392} avatar='https://bit.ly/dan-abramov' />
-        <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence' />
-        <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast' />
-  
-        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-          © 2023 Built By{" "}
-          <Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
-            As a Programmer
-          </Link>
-        </Box>
-      </VStack>
-    );
-  };
-  
+      )}
+
+      {suggestedUsers.map(user => (
+        <SuggestedUser user={user} key={user.id} />
+      ))}
+
+      <Box fontSize={12} color={'gray.500'} mt={5} alignSelf={'start'}>
+        © 2023 Built By{' '}
+        <Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
+          As a Programmer
+        </Link>
+      </Box>
+    </VStack>
+  );
+};
 
 export default SuggestedUsers;
