09df4c4b2619ad7a0989fa4a8746e4a16ce792ee
Mohammad Al-Buraiki <alburaeki@gmail.com>
2025-01-16 21:25:03 -0500

    bug fixes and improvements

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,35 +1,55 @@
-import { Box, Container, Flex, VStack } from '@chakra-ui/react'
-import AuthForm from '../../components/AuthForm/AuthForm'
+import { Box, Container, Flex, VStack, Link, Image } from '@chakra-ui/react';
+import AuthForm from '../../components/AuthForm/AuthForm';
+import { useTranslation } from 'react-i18next'; // Added
 const AuthPage = () => {
+  const { t, i18n } = useTranslation(); // Added
   return (
-<Flex minH={'100vh'} alignItems={'center'} justifyContent={'center'} px={4}>
-  <Container maxW={'container.md'} padding={0}>
-    <Flex justifyContent={'center'} alignItems={'center'} gap={'10'}>
+    <Flex minH={'100vh'} direction='column' px={4} alignItems={'center'}>
+      {/* Built By Section */}
+      <Flex
+        fontSize={14}
+        color={'gray.500'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        mb={1} // Adds space below the "Built by" section
+        marginTop={5}
+      >
+        ❖ {t('built_by')} {''}
+        <Link
+          href='https://github.com/Mohammad-al-buraiki'
+          target='_blank'
+          color='blue.500'
+          fontSize={14}
+          display='inline-flex'
+          alignItems='center'
+          marginLeft={1.5}
+        >
+          {t('author_name')}
+          <Image src='/github-logo.png' alt='GitHub Logo' boxSize='16px' ml={2} cursor={'pointer'} />
+        </Link>
+      </Flex>
 
-      {/* left side */}
-      <Box display={{ base: "none", md: "block" }}>
-        <img
-          src="/auth.png"
-          style={{ height: '650px' }} // Corrected this part
-          alt="Phone img"
-        />
-      </Box>
+      {/* Main Auth Content */}
+      <Container maxW={'container.md'} padding={0}>
+        <Flex justifyContent={'center'} alignItems={'center'} gap={'10'}>
+          {/* Left side */}
+          <Box display={{ base: 'none', md: 'block' }}>
+            <img src='/auth.png' style={{ height: '650px' }} alt='Phone img' />
+          </Box>
 
-      {/* right side */}
-      <VStack spacing={4} align={'stretch'}>
-        <AuthForm />
-        <Box textAlign={'center'}> Get the app.</Box>
-        <Flex gap={5} justifyContent={'center'}>
-          <img src="/microsoft.png" style={{ height: '40px' }} alt="Appstore logo" /> {/* Adjusted height */}
-          <img src='/playstore.png' style={{ height: '40px' }} alt="Playstore logo" /> {/* Adjusted height */}
+          {/* Right side */}
+          <VStack spacing={4} align={'stretch'}>
+            <AuthForm />
+            <Box textAlign={'center'}> Get the app.</Box>
+            <Flex gap={5} justifyContent={'center'}>
+              <img src='/microsoft.png' style={{ height: '40px' }} alt='Appstore logo' />
+              <img src='/playstore.png' style={{ height: '40px' }} alt='Playstore logo' />
+            </Flex>
+          </VStack>
         </Flex>
-      </VStack>
-
+      </Container>
     </Flex>
-  </Container>
-</Flex>
+  );
+};
 
-  )
-}
-
-export default AuthPage
+export default AuthPage;
